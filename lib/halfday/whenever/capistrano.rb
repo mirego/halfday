447a1192a9242f6caef752cd11ae12e310c6d364
Samuel Garneau <sgarneau@mirego.com>
2013-06-19 16:07:39 -0400

    Update whenever callback

--- lib/halfday/whenever/capistrano.rb
+++ lib/halfday/whenever/capistrano.rb
@@ -3,10 +3,10 @@ Capistrano::Configuration.instance(:must_exist).load do
   begin
     require 'whenever/capistrano'
 
-    before 'whenever:update_crontab', 'bundle:install'
+    after 'bundle:install', 'whenever:update_crontab'
 
-    _cset :whenever_environment, defer { stage }
-    _cset :whenever_command, "bundle exec whenever"
+    set :whenever_environment, defer { rails_env }
+    set :whenever_command, "bundle exec whenever"
   rescue LoadError
     puts "WARNING: Whenever is not part of the application"
   end
