54dfc1ce66e0dbe5489adf18c9cb8ab064d89273
Samuel Garneau <sgarneau@mirego.com>
2013-06-20 13:24:21 -0400

    Stop running yard:generate unless Yard is defined

--- lib/halfday/yard/capistrano.rb
+++ lib/halfday/yard/capistrano.rb
@@ -1,17 +1,19 @@
 Capistrano::Configuration.instance(:must_exist).load do
 
-  after 'deploy:create_symlink', 'yard:generate'
+  if defined?(Yard)
+    after 'deploy:create_symlink', 'yard:generate'
 
-  namespace :yard do
+    namespace :yard do
 
-    desc "Update the documentation alone before a release`"
-    task :generate, :roles => :app do
-      # Build the documentation for the application and make it public
-      run "mkdir #{current_path}/doc"
-      run "cd #{current_path} && #{bundle_cmd} exec yard doc RAILS_ENV=#{stage}"
-      run "mv #{current_path}/doc #{current_path}/public/doc"
+      desc "Update the documentation alone before a release`"
+      task :generate, :roles => :app do
+        # Build the documentation for the application and make it public
+        run "mkdir #{current_path}/doc"
+        run "cd #{current_path} && #{bundle_cmd} exec yard doc RAILS_ENV=#{stage}"
+        run "mv #{current_path}/doc #{current_path}/public/doc"
+      end
+
     end
-
   end
 
 end
