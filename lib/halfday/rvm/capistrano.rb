6b007d5496360d878e0610c47a43f1213263baa1
Samuel Garneau <sgarneau@mirego.com>
2013-06-20 11:39:03 -0400

    Fix issue with RVM not being loaded correctly for Bundler

--- lib/halfday/rvm/capistrano.rb
+++ lib/halfday/rvm/capistrano.rb
@@ -4,6 +4,10 @@ Capistrano::Configuration.instance(:must_exist).load do
     require 'rvm/capistrano'
 
     _cset :rvm_ruby_string, :release_path
+
+    # Make sure that the bash_profile is sourced
+    # before running bundle command.
+    set :bundle_cmd, 'source $HOME/.bash_profile && bundle'
   rescue LoadError
     puts "WARNING: RVM is not part of the application"
   end
