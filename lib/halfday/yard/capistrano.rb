00818c02cee74ceadf5d8cfa5b6b72ef25d00ff6
Samuel Garneau <sgarneau@mirego.com>
2013-06-20 11:59:38 -0400

    Use bundle_cmd for Yard module

--- lib/halfday/yard/capistrano.rb
+++ lib/halfday/yard/capistrano.rb
@@ -8,7 +8,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :generate, :roles => :app do
       # Build the documentation for the application and make it public
       run "mkdir #{current_path}/doc"
-      run "cd #{current_path} && bundle exec yard doc RAILS_ENV=#{stage}"
+      run "cd #{current_path} && #{bundle_cmd} exec yard doc RAILS_ENV=#{stage}"
       run "mv #{current_path}/doc #{current_path}/public/doc"
     end
 
